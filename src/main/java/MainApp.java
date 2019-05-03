4616f6d48f2a10b8aa7bba0a8a474c1a4d1c33bd
Alexa Drenick <adrenick@calpoly.edu>
2019-05-02 22:21:03 -0700

    start of project

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) {
+        Platform.setImplicitExit(false);
+        this.primaryStage = stage;
+        this.primaryStage.setTitle("MoneyMinder");
+        //this.primaryStage.setResizable(false);
+        initializeScene();
+    }
+
+    private void initializeScene() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("FXML/Login.fxml"));
+            Pane layout = (Pane) loader.load();
+            Scene landingScene = new Scene(layout);
+            primaryStage.setScene(landingScene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
