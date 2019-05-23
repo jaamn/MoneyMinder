1f8fd39b1377e052b68ab136d007a77145af80d6
aju960219 <aju960219@gmail.com>
2019-05-22 19:32:02 -0700

    Separate register page and few edit on the register page

--- src/main/java/Controllers/LoginController.java
+++ src/main/java/Controllers/LoginController.java
@@ -39,7 +39,7 @@ public class LoginController {
         });
 
         registerButton.setOnAction( event -> {
-            ((Node)(event.getSource())).getScene().getWindow().hide();
+//            ((Node)(event.getSource())).getScene().getWindow().hide();
             switchToRegisterScene();
         });
 
