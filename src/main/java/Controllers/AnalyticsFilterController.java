623738e343ac1d2f3398fbc62f2d35ed928ce29a
Alexa Drenick <adrenick@calpoly.edu>
2019-05-30 18:39:17 -0700

    controller files

---
+++ src/main/java/Controllers/AnalyticsFilterController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import Models.User;
+
+public class AnalyticsFilterController {
+
+    private User user;
+
+    public AnalyticsFilterController(User user)
+    {
+        this.user = user;
+    }
+}
