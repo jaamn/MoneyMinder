e22693ea312ad563ec3f8933e79af4b289b519b6
aju960219 <aju960219@gmail.com>
2019-06-05 12:05:31 -0700

    Integration test example

---
+++ src/test/java/IntegrationTest/CategoryIT.java
@@ -0,0 +1,21 @@
+package IntegrationTest;
+
+import Models.Category;
+import Models.Tables;
+import Utils.SQL.QueryFactory.InsertQueryFactory;
+import Utils.SQL.QueryStatements.InsertQueries.InsertQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryIT {
+    @Test
+    void TestinsertIntoDB(){
+        Category category = new Category(1, "Groceries");
+        InsertQuery insertIntoCategories;
+        category.insertIntoDB();
+        insertIntoCategories = InsertQueryFactory.getQuery(Tables.Categories);
+        Boolean truth = insertIntoCategories.execute(category);
+        Assertions.assertNotNull(insertIntoCategories, "insertQueryFactory for categories is null");
+        Assertions.assertTrue(truth, "InsertionintoCategories executed");
+    }
+}
