dd244fb296d95ce067ec39e6fdbb80074cc8f4fb
aju960219 <aju960219@gmail.com>
2019-06-04 19:45:39 -0700

    unittest

---
+++ src/test/java/UnitTest/Models/CategoryTest.java
@@ -0,0 +1,18 @@
+package UnitTest.Models;
+import Models.Category;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
+public class CategoryTest {
+    @Test
+    void TestgetPresetCategories(){
+        List<Category> categoryList = Category.getPresetCategories();
+        Assertions.assertNotNull(categoryList);
+
+        Assertions.assertEquals("Groceries", categoryList.get(0).getName());
+        Assertions.assertEquals("1", categoryList.get(0).getCid());
+    }
+
+}
