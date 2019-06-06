5921476cd504ec12e218d7c8de40bfe6caa36ea0
aju960219 <aju960219@gmail.com>
2019-06-06 15:24:58 -0700

    hash password uniitest

--- src/test/java/Utils/PasswordHashTest.java
+++ src/test/java/Utils/PasswordHashTest.java
@@ -1,5 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package Utils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PasswordHashTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void get_SHA_256_SecurePassword() {
+        String password = "1234567";
+        String salt = "pizza";
+        String hashedPassword = PasswordHash.get_SHA_256_SecurePassword(password, salt);
+        Assertions.assertNotEquals(hashedPassword, password, "hashed password is different");
+    }
 }
