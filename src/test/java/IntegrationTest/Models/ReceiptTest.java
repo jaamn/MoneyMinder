caa6ff4fc9b8cae28d252b90505260ed542c8478
Alexa Drenick <adrenick@calpoly.edu>
2019-06-06 16:20:50 -0700

    db creation

--- src/test/java/IntegrationTest/Models/ReceiptTest.java
+++ src/test/java/IntegrationTest/Models/ReceiptTest.java
@@ -80,6 +80,13 @@ class ReceiptTest {
         if(database.exists()) {
             System.out.println("DATABASE EXISTS");
         }
+        else
+        {
+            System.err.println("DATABASE DOESNT EXIST");
+            Main.MainApp M = new Main.MainApp();
+            M.initDB();
+
+        }
         Receipt r = new Receipt(2, 2, "Test2", Date.valueOf("2019-06-06"));
         InsertQuery insert = InsertQueryFactory.getQuery(Tables.Receipts);
         Assertions.assertTrue(insert.execute(r));
