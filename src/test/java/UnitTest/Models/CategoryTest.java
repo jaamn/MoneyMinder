9447ba381fd8b8535f6388cf73a3ceacbab9c435
mathurma <mathurma@calpoly.edu>
2019-06-06 07:56:15 -0700

    Category: 100% class covererage 88% method covereage 90% line covereage

--- src/test/java/UnitTest/Models/CategoryTest.java
+++ src/test/java/UnitTest/Models/CategoryTest.java
@@ -1,14 +1,65 @@
 package UnitTest.Models;
 import Models.Category;
+import Models.Tables;
+import Utils.SQL.QueryFactory.InsertQueryFactory;
+import Utils.SQL.QueryStatements.InsertQueries.InsertQuery;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.io.File;
 import java.util.List;
 
 public class CategoryTest {
 
+    @BeforeEach
+    void setUp() {
+        File database = new File((System.getProperty("user.dir") + "\\database.db"));
+        database.delete();
+        Main.MainApp M = new Main.MainApp();
+        M.initDB();
+    }
+
+    @AfterEach
+    void tearDown() {
+        File database = new File((System.getProperty("user.dir") + "\\database.db"));
+        database.delete();
+    }
+
+//    @Test
+//    void toString() {
+//    }
+
     @Test
-    void TestgetPresetCategories(){
+    void insertIntoDB() {
+        Category c = new Category(7, "Gifts");
+        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Categories);
+        Assertions.assertTrue(insert.execute(c));
+    }
+
+    @Test
+    void insertPresetCategoriesIntoDB() {
+    }
+
+    @Test
+    void fieldsForTableCreation() {
+    }
+
+    @Test
+    void getCid() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCategories() {
+    }
+
+    @Test
+    void getPresetCategories() {
         List<Category> categoryList = Category.getPresetCategories();
         Assertions.assertNotNull(categoryList);
         Assertions.assertEquals("Groceries", categoryList.get(0).getName());
