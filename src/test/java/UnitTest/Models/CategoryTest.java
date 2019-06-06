9d8f4a0102608b585ba364ce4000dd26440e4ef4
mathurma <mathurma@calpoly.edu>
2019-06-06 14:27:35 -0700

    added commentary

--- src/test/java/UnitTest/Models/CategoryTest.java
+++ src/test/java/UnitTest/Models/CategoryTest.java
@@ -13,63 +13,25 @@ import java.util.List;
 
 public class CategoryTest {
 
-    @BeforeEach
-    void setUp() {
-//        File database = new File("database.db");
-//        if(database.exists()) {
-//            database.delete();
-//        }
-//        Main.MainApp M = new Main.MainApp();
-//        M.initDB();
-    }
-
-    @AfterEach
-    void tearDown() {
-//        File database = new File((System.getProperty("user.dir") + "\\database.db"));
-//        if(database.exists()) {
-//            database.delete();
-//        }
-    }
-
-    /* can't override */
-//    @Test
-//    void toString() {
-//    }
-
-    /* moved to integration */
-//    @Test
-//    void insertIntoDB() { //Integration
-//        Category c = new Category(7, "Gifts");
-//        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Categories);
-//        Assertions.assertTrue(insert.execute(c));
-//    }
-
-    /* covered by others */
-//    @Test
-//    void insertPresetCategoriesIntoDB() {
-//    }
-//
-//    @Test
-//    void fieldsForTableCreation() {
-//    }
-//
-//    @Test
-//    void getCid() {
-//    }
-//
-//    @Test
-//    void getName() {
-//    }
-//
-//    @Test
-//    void getCategories() {
-//    }
-
     @Test
     void getPresetCategories() {
+        /*  The following Unit Test tests getPresetCategories(),
+            - a function in the class Category
+            - preps default fields for database
+         */
         List<Category> categoryList = Category.getPresetCategories();
         Assertions.assertNotNull(categoryList);
         Assertions.assertEquals("Groceries", categoryList.get(0).getName());
         Assertions.assertEquals(1, categoryList.get(0).getCid());
+        Assertions.assertEquals("Personal", categoryList.get(1).getName());
+        Assertions.assertEquals(2, categoryList.get(1).getCid());
+        Assertions.assertEquals("Entertainment", categoryList.get(2).getName());
+        Assertions.assertEquals(3, categoryList.get(2).getCid());
+        Assertions.assertEquals("Transportation", categoryList.get(3).getName());
+        Assertions.assertEquals(4, categoryList.get(3).getCid());
+        Assertions.assertEquals("Bills", categoryList.get(4).getName());
+        Assertions.assertEquals(5, categoryList.get(4).getCid());
+        Assertions.assertEquals("One-offs", categoryList.get(5).getName());
+        Assertions.assertEquals(6, categoryList.get(5).getCid());
     }
 }
