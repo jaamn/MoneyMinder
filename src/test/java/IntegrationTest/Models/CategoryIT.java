50e220f9d7c3b34aa50529515ed51d44b54c0e59
aju960219 <aju960219@gmail.com>
2019-06-09 12:45:41 -0700

    rename CategoryTest to CategoryIT

---
+++ src/test/java/IntegrationTest/Models/CategoryIT.java
@@ -0,0 +1,47 @@
+package IntegrationTest.Models;
+
+import Models.Category;
+import Models.Tables;
+import Utils.SQL.QueryFactory.InsertQueryFactory;
+import Utils.SQL.QueryStatements.InsertQueries.InsertQuery;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryIT {
+
+    @BeforeEach
+    void setUp() {
+        File database = new File("database.db");
+        if(database.exists()) {
+            database.delete();
+        }
+        Main.MainApp M = new Main.MainApp();
+        M.initDB();
+    }
+
+    @AfterEach
+    void tearDown() {
+        File database = new File("database.db");
+        if(database.exists()) {
+            database.delete();
+        }
+    }
+
+    @Test
+    void insertIntoDB() {
+        /*  The following Integration Test tests insertIntoDB(),
+            - a function in the class Category
+            - integration of SQL operation, enum values, factory operation
+            - inserts specific category into database
+         */
+        Category c = new Category(7, "Gifts");
+        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Categories);
+        Assertions.assertTrue(insert.execute(c));
+    }
+}
