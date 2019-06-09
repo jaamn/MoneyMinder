255009570fc4f11d45e74ee6f0881a4f7691b803
mathurma <mathurma@calpoly.edu>
2019-06-09 14:30:42 -0700

    restored non-SQL specific error check

--- src/main/java/Utils/SQL/QueryStatements/InsertQueries/InsertIntoReceipts.java
+++ src/main/java/Utils/SQL/QueryStatements/InsertQueries/InsertIntoReceipts.java
@@ -24,16 +24,16 @@ public class InsertIntoReceipts implements InsertQuery {
             String insert = sj.toString();
             System.out.println(insert);
             stmt.executeUpdate(insert);
-        } catch (ClassNotFoundException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
             System.err.println(e.getMessage());
             return false;
-        } catch (SQLException s) {
+        } /*catch (SQLException s) {
             System.err.println(s.getLocalizedMessage());
             if (s.getErrorCode() == 19) return true; // PRIMARY KEY constraint failed (how do we check which key?)
             // UNIQUE KEY failure is okay^ (normally duplicate will not be inserted, so false)
             return false;
-        }
+        }*/
         return true;
     }
 }
