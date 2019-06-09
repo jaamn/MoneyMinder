7fa53ee4f9bf05e9c75523f176a187b279d4aec3
mathurma <mathurma@calpoly.edu>
2019-06-09 16:00:43 -0700

    tested last method

--- src/test/java/IntegrationTest/Models/ReceiptIT.java
+++ src/test/java/IntegrationTest/Models/ReceiptIT.java
@@ -22,61 +22,83 @@ class ReceiptIT {
     Receipt r = new Receipt(rid, sid, "Test1", Date.valueOf("2019-06-06"));
     Receipt r2 = new Receipt(2, 2, "Test2", Date.valueOf("2019-06-06"));
     Item i = new Item(1, 1, "TestItem", (float)5.0, 1);
-        @BeforeEach
-        void setUp() {
-            File database = new File("database.db");
-            Main.MainApp M = new Main.MainApp();
-            if(database.exists()) {
-                M.deleteTables();
-            }
-            M.initDB();
-        }
+    Item i2 = new Item(1, 1, "TestItem2", (float)7.0, 1);
+    Item i3 = new Item(1, 1, "TestItem3", (float)2.0, 1);
 
-        @BeforeEach
-        void tearDown() {
-            Main.MainApp M = new Main.MainApp();
+
+
+    @BeforeEach
+    void setUp() {
+        File database = new File("database.db");
+        Main.MainApp M = new Main.MainApp();
+        if(database.exists()) {
             M.deleteTables();
         }
-
-        @Test
-        void getStoreName() {
-            InsertQuery insertS = InsertQueryFactory.getQuery(Tables.Stores);
-            insertS.execute("Village Market");
-
-            InsertQuery insert = InsertQueryFactory.getQuery(Tables.Users);
-            insert.execute(u);
-
-            InsertQuery insertR = InsertQueryFactory.getQuery(Tables.Receipts);
-            insertR.execute(r);
-
-            Assertions.assertEquals("Village Market", r.getStoreName().getValue());
-        }
-
-        @Test
-        void insertIntoDB() {
-            InsertQuery insert = InsertQueryFactory.getQuery(Tables.Receipts);
-            Assertions.assertTrue(insert.execute(r2));
-        }
-
-        @Test
-        void getSpendingForMonth() {
-            InsertQuery insertS = InsertQueryFactory.getQuery(Tables.Stores);
-            insertS.execute("Village Market");
-
-            InsertQuery insert = InsertQueryFactory.getQuery(Tables.Users);
-            insert.execute(u);
-
-            InsertQuery insertR = InsertQueryFactory.getQuery(Tables.Receipts);
-            insertR.execute(r);
-
-            InsertQuery insertItem = InsertQueryFactory.getQuery(Tables.Items);
-            insertItem.execute(i);
-
-            Assertions.assertEquals((float)5.0, r.getSpendingForMonth(u,"06", "2019"));
-        }
-
-//    @Test
-//    void getSpendingPerCategoryForMonth() {
-//
-//    }
+        M.initDB();
     }
+
+    @BeforeEach
+    void tearDown() {
+        Main.MainApp M = new Main.MainApp();
+        M.deleteTables();
+    }
+
+    @Test
+    void getStoreName() {
+        InsertQuery insertS = InsertQueryFactory.getQuery(Tables.Stores);
+        insertS.execute("Village Market");
+
+        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Users);
+        insert.execute(u);
+
+        InsertQuery insertR = InsertQueryFactory.getQuery(Tables.Receipts);
+        insertR.execute(r);
+
+        Assertions.assertEquals("Village Market", r.getStoreName().getValue());
+    }
+
+    @Test
+    void insertIntoDB() {
+        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Receipts);
+        Assertions.assertTrue(insert.execute(r2));
+    }
+
+    @Test
+    void getSpendingForMonth() {
+        InsertQuery insertS = InsertQueryFactory.getQuery(Tables.Stores);
+        insertS.execute("Village Market");
+
+        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Users);
+        insert.execute(u);
+
+        InsertQuery insertR = InsertQueryFactory.getQuery(Tables.Receipts);
+        insertR.execute(r);
+
+        InsertQuery insertItem = InsertQueryFactory.getQuery(Tables.Items);
+        insertItem.execute(i);
+
+        Assertions.assertEquals((float)5.0, r.getSpendingForMonth(u,"06", "2019"));
+    }
+
+    @Test
+    void getSpendingPerCategoryForMonth() {
+        InsertQuery insertS = InsertQueryFactory.getQuery(Tables.Stores);
+        insertS.execute("Village Market");
+
+        InsertQuery insert = InsertQueryFactory.getQuery(Tables.Users);
+        insert.execute(u);
+
+        InsertQuery insertR = InsertQueryFactory.getQuery(Tables.Receipts);
+        insertR.execute(r);
+
+        InsertQuery insertItem = InsertQueryFactory.getQuery(Tables.Items);
+        insertItem.execute(i);
+        insertItem.execute(i2);
+        insertItem.execute(i3);
+
+        Assertions.assertEquals((float)14,
+                r.getSpendingPerCategoryForMonth(u, new Category(1, "Groceries"),"06", "2019"));
+        Assertions.assertEquals((float)0,
+                r.getSpendingPerCategoryForMonth(u, new Category(1, "Groceries"),"07", "2019"));
+    }
+}
